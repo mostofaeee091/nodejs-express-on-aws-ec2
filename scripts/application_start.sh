527c7b23a5e2bb6327268d4ed185dcc70f583703
devmostofa <mostofaeee091@gmail.com>
2022-06-29 15:08:14 +0600

    fixed some info

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -4,7 +4,7 @@
 sudo chmod -R 777 /home/ec2-user/express-app
 
 #navigate into our working directory where we have all our github files
-cd /home/ec2-user/express-app
+cd /home/ubuntu/express-app
 
 #add npm and node to path
 export NVM_DIR="$HOME/.nvm"	
